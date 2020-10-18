5fe120c48ece68d70f6176999dc3807a44ad41e4
Indranil Tiwary <>
2020-10-19 02:42:22 +0530

    Express Validation added

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 
 const router = Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Hi There');
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Please enter a valid email.'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req, res) => {
+    res.send('Hello');
+  }
+);
 
 export { router as signupRouter };
