7fe167322dceafe8fc1d5671a16e95e8524c002f
Indranil Tiwary <indraniltiwary@hotmail.com>
2020-10-27 22:23:48 +0530

    Test written

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,28 +1,28 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import { app } from '../app';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
 
 let mongo: any;
 beforeAll(async () => {
-    process.env.JWT_KEY = 'sdnjaksnd';
-    mongo = new MongoMemoryServer();
-    const mongoUri = await mongo.getUri();
+  process.env.JWT_KEY = "sdnjaksnd";
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
 
-    await mongoose.connect(mongoUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 });
 
 beforeEach(async () => {
-    const collections = await mongoose.connection.db.collections();
+  const collections = await mongoose.connection.db.collections();
 
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
 });
 
 afterAll(async () => {
-    await mongo.stop();
-    await mongoose.connection.close();
+  await mongo.stop();
+  await mongoose.connection.close();
 });
